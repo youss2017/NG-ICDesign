f0f21212e41ada9a8da8841e71b29006cbeefaf4
Pablo Rodriguez <pablo.rodriguez@ucf.edu>
2025-04-08 22:34:19 -0400

    changed compiler flag settings for basys3 soc

--- Pablo_Rodriguez/rapidx-software/main.c
+++ Pablo_Rodriguez/rapidx-software/main.c
@@ -4,6 +4,7 @@
  * section will be placed at the start of RAM by the linker.
  */
 void _rapidx_start(void) __attribute__ (( noreturn, section(".text.rapidx.entry") ));
+void (*const bootloader)(void) __attribute__ (( noreturn )) = 0;
 
 void uart_puts(const char* s) {
     while(*s != '\0') {
@@ -12,13 +13,51 @@ void uart_puts(const char* s) {
     }
 }
 
-void _rapidx_start(void) {
-    for(;;) {
-        int a = (GPIO_SW & 0x00FF);
-        int b = (GPIO_SW & 0xFF00) >> 8;
-        GPIO_LED = a * b;
-        uart_puts("hello world! this is rapidx\r\n");
-    }
+void uart_gets(char *s) {
+    do {
+        while(!(UART_STAT & RXREADY));
+        *s = UART_RX;
+    } while(*s++ != '\n');
+    *s = '\0';
+}
 
-    for(;;) {}
+const char* word2hex(int word) {
+    static char hex[9];
+    char hexmap[] = "0123456789ABCDEF";
+    for(int i = 7; i >= 0; i--) {
+        hex[i] = hexmap[word & 0x0F];
+        word >>= 4;
+    }
+    hex[8] = '\0';
+    return hex;
+}
+
+int factorial(int b) {
+
+    int read_sp;
+    asm (
+        "add %0, sp, zero;"
+        : "=r"(read_sp)
+    );
+
+    uart_puts("factorial(");
+    uart_puts(word2hex(b));
+    uart_puts(") ; sp = ");
+    uart_puts(word2hex(read_sp));
+    uart_puts("\r\n");
+
+    if(b == 1) return 1;
+    else return b * factorial(b - 1);
+}
+
+extern char _rapidx_ram_base;
+void _rapidx_start(void) {
+
+    int f = factorial(8);
+    uart_puts("f = ");
+    uart_puts(word2hex(f));
+    uart_puts("\r\n");
+
+    bootloader();
+
 }
